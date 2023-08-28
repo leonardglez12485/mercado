ceff6a3949a0f4bbd39233ad14b8d279dc63312b
Leonard <leonardglez12485@gmail.com>
2023-08-28 09:42:36 -0500

    Habilitanod Cors

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/docs', app, document);
+  app.enableCors();
   await app.listen(process.env.PORT);
   console.log(`App Running al Port ${process.env.PORT}`)
 }
