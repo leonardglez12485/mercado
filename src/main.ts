7dc0015608ab4dceb92f1156d7066b933dee5f6f
Leonard <leonardglez12485@gmail.com>
2023-08-29 09:44:00 -0500

    Modificando la ruta de acceso

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api/docs');
   //validacion de los Pipes
   app.useGlobalPipes(
     new ValidationPipe({
--- src/mercancia/mercancia.module.ts
+++ src/mercancia/mercancia.module.ts
@@ -1,18 +1,17 @@
-import { Controller, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { MercanciaService } from './mercancia.service';
 import { MercanciaController } from './mercancia.controller';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Departamento, DepartamentoSchema } from 'src/departamento/entities/departamento.entity';
 import { Mercancia, MercanciaSchema } from './entities/mercancia.entity';
 import { DepartamentoModule } from 'src/departamento/departamento.module';
-import { DepartamentoService } from '../departamento/departamento.service';
-import { Model } from 'mongoose';
+
 
 @Module({
   controllers: [MercanciaController],
-  providers: [MercanciaService, ConfigService],
+  providers: [MercanciaService],
   imports:[
+    ConfigModule,
     MongooseModule.forFeature([
       {
        name: Mercancia.name,
